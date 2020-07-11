bbf5a7694fabe30cf8772c4a3e77cb4ba8082b60
knigamaxim <maxwpdev@gmail.com>
2020-07-11 21:44:52 +0200

    Update Db.php

--- Db.php
+++ Db.php
@@ -3,9 +3,12 @@
  */
 class Db
 {
+	
+ 	use TSingleton;
+	
 	protected $dbh;
 	
-	public function __construct()
+	protected function __construct()
 	{
 		$this->dbh = new PDO('mysql:host=127.0.0.1;dbname=foo', 'root', '');
 	}
